eafb471206e169b4536b47b90f911e6a3d9aec90
Bolaji Olajide <cooproton@gmail.com>
2019-06-05 23:20:07 +0100

    create simple flask app

---
+++ main.py
@@ -0,0 +1,25 @@
+from datetime import date
+from time import gmtime, strftime
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return jsonify({"message": "Welcome to my Flask App"})
+
+
+@app.route('/info')
+def info():
+  return jsonify({
+    "current_date": date.today().strftime("%B %d, %Y"),
+    "current_time": strftime("%H:%M:%S +0000", gmtime()),
+    "status": "OK"
+  })
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
